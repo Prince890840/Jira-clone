1d24f4d97be91b22888fa249662cecc3a8ad0b47
Prince890840 <princepanchani890@gmail.com>
2023-02-20 22:26:22 +0530

    Header Scroll event Improvement

---
+++ main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
